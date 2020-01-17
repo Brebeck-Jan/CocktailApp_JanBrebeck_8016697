d35fe63c621516db89d2ae512085692c4de223db
Jan Brebeck <Jan.Brebeck98@gmail.com>
2020-01-17 16:00:39 +0100

    created the router

---
+++ cocktail_app/src/containers/receipts.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./receipts";
+
+class Receipts extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    
+    render() {
+        return (
+            <div>
+                Receiptcomponent
+            </div>
+        );
+    }
+}
+
+export default Receipts;
