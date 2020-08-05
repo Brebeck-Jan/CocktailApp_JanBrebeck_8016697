dc3b8920aa3bea4398e0ae58f20561847b809f78
Jan Brebeck <Jan.Brebeck98@gmail.com>
2020-08-05 08:37:01 +0200

    Updates

---
+++ precache-manifest.ec59b20e17bc59a12c19f45528987f71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cece7549143c836220d678c0e6ca86cf",
+    "url": "/CocktailApp_JanBrebeck_8016697/index.html"
+  },
+  {
+    "revision": "636355f6498f5cfa727d",
+    "url": "/CocktailApp_JanBrebeck_8016697/static/css/main.3a8943b6.chunk.css"
+  },
+  {
+    "revision": "f78ecc2de1301590092d",
+    "url": "/CocktailApp_JanBrebeck_8016697/static/js/2.221a095c.chunk.js"
+  },
+  {
+    "revision": "b5eb84ccd29afb0aa551b261e39535e9",
+    "url": "/CocktailApp_JanBrebeck_8016697/static/js/2.221a095c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "636355f6498f5cfa727d",
+    "url": "/CocktailApp_JanBrebeck_8016697/static/js/main.241a3e25.chunk.js"
+  },
+  {
+    "revision": "8f5e648eafd66b86e7cd",
+    "url": "/CocktailApp_JanBrebeck_8016697/static/js/runtime-main.c0aaea89.js"
+  },
+  {
+    "revision": "a73750b00f0f7002e9504967277882f1",
+    "url": "/CocktailApp_JanBrebeck_8016697/static/media/Background.a73750b0.jpg"
+  }
+]);
